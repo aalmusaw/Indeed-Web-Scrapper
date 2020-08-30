1c5b1a72c0e7ba02790d9a92e9c9e109f2f3c33a
Ali Al-Musawi <54688322+aalmusaw@users.noreply.github.com>
2020-08-30 15:40:40 -0400

    Update main.py
    Fixed path concatenation on line 44 so it's platform-independent

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 """This module contains the user interface"""
+import os
+import pathlib
+
 import scrapper
 import url_utilities
+
 
 def main():
     print("Welcome to Indeed Job Search Tool")
@@ -37,7 +41,7 @@ def main():
     if "y" in response.lower():
         response = input("Provide a directory path to write to: ")
         try:
-            f = open(response+"job_query.csv", "w")
+            f = open(os.join(pathlib.Path(response), pathlib.Path("job_query.csv"), "w")
             f.write(str(query))
             f.close()
             print("File written successfully.")
